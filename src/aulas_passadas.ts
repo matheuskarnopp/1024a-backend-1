0435821ec8ae2d4ebd34307b139bd5d535a64510
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2026-03-17 07:50:40 -0400

    adição da aula passa no arquivo "aulas_passadas", e codigo usando spread operator

--- src/aulas_passadas.ts
+++ src/aulas_passadas.ts
@@ -27,22 +27,43 @@
 //após 5 segundos ou a função devolve o valor null null
 // function devolveTere()
 
-async function devolveTere():Promise<string | null> { 
-    const returnPromisse = new Promise<string | null>((resolve, reject) => {
+// async function devolveTere():Promise<string | null> { 
+//     const returnPromisse = new Promise<string | null>((resolve, reject) => {
         
-        if (Math.random()>.5) {
-            setTimeout(() => { resolve("Tere")}, 5000)
-        }else{
-            reject(null)
-        }
+//         if (Math.random()>.5) {
+//             setTimeout(() => { resolve("Tere")}, 5000)
+//         }else{
+//             reject(null)
+//         }
        
-    })
-    return returnPromisse
-}
+//     })
+//     return returnPromisse
+// }
 
-try {
-    const resultado = await devolveTere()
-    console.log(resultado)    
-} catch (erro) {
-    console.log(erro)
-}
+// try {
+//     const resultado = await devolveTere()
+//     console.log(resultado)    
+// } catch (erro) {
+//     console.log(erro)
+// }
+
+
+// // const vetor:number[] = [1,2,3,4]
+// const vetor:{id:number, nome:string,idade:number}[] = [
+//     {id:1,nome:"tere",idade:32},
+//     {id:2,nome:"Marcelo",idade:17},
+//     {id:3,nome:"Joãozinho",idade:18},
+// ]
+
+// //find
+// //const resultado = vetor.find((x) => x.idade >= 18)
+// //console.log(resultado)
+// // console.log(vetor.find((x) => x==5))
+
+// //filter
+// // const resultado2 = vetor.filter((x) => x.idade >= 18) 
+// // console.log(resultado2)
+
+// //map
+// const resultado3 = vetor.map((x) => x.idade) 
+// console.log(resultado3)
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
-// const vetor:number[] = [1,2,3,4]
-const vetor:{id:number, nome:string,idade:number}[] = [
-    {id:1,nome:"tere",idade:32},
-    {id:2,nome:"Marcelo",idade:17},
-    {id:3,nome:"Joãozinho",idade:18},
-]
+const x = [1,2,3,4]
+const y = [5,6,7,8]
+const z = [...x, 4.5]
+console.log(z)
+const xy = [...x, ...y]
+console.log(xy)
 
-//find
-//const resultado = vetor.find((x) => x.idade >= 18)
-//console.log(resultado)
-// console.log(vetor.find((x) => x==5))
-
-//filter
-// const resultado2 = vetor.filter((x) => x.idade >= 18) 
-// console.log(resultado2)
-
-//map
-const resultado3 = vetor.map((x) => x.idade) 
-console.log(resultado3)
+const max = Math.max(...x)
+console.log(max)
