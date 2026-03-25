3564007dbfaaa41c7bff0bad586d9556d5d534df
Vitor <vitorluciopadilhaoliveira4@gmail.com>
2026-03-24 20:44:21 -0400

    separação de responsabilidades de rotas

--- src/index.ts
+++ src/index.ts
@@ -6,45 +6,10 @@ import express from 'express';
 const app = express()
 // app.use(express.json())
 //como criar uma rota no express?
-app.get("/pessoas", async(req,res)=>{
-  
 
-  let connection:Connection | null = null
-  // Create the connection to database
-  try {
-  const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'luademel',
-});
-
-// Using placeholders
-
-  // const [dados, campos] = await connection.execute('SELECT * FROM pessoa')
-  // const result = await connection.execute('INSERT INTO pessoa (id, nome) values (?, ?)', [3, "Marcelo"])
-  // console.log(result)
-  // console.log(dados);
-  
-  
-  const [dados, campos] = await connection.execute<IPessoa[]>('SELECT * FROM pessoa')
-  res.status(200).json(dados)
-} catch (err) {
-  if (connection) {
-    await (connection as Connection).end()
-}
-}
-
-
-})
-app.post("/pessoas", ()=>{
-  
-})
 
 app.listen(8000, ()=>{
   console.log("Iniciando o servidor na porta 8000")
 })
 
-interface IPessoa extends RowDataPacket{
-  id:number,
-  nome:"string",
-}
+
--- src/routes.ts
+++ src/routes.ts
@@ -1,2 +1,40 @@
 import { Router } from "express";
+import mysql, { type RowDataPacket, type Connection  } from 'mysql2/promise';
 const routes = Router()
+
+
+
+////////////
+interface IPessoa extends RowDataPacket{
+  id:number,
+  nome:"string",
+}
+
+////////////
+routes.get("/pessoas", async(req,res)=>{
+
+  let connection:Connection | null = null
+  // Create the connection to database
+  try {
+  const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'luademel',
+  })  
+  const [dados, campos] = await connection.execute<IPessoa[]>('SELECT * FROM pessoa')
+//   res.status(200).json(dados)
+} 
+  catch (err) {
+  if (connection) {
+    await (connection as Connection).end()
+}
+}
+
+});
+
+
+routes.post("/pessoas", ()=>{
+  
+})
+
+export default routes
