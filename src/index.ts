a7acebf20fffc389ba4c5393b36569bb3e40a2aa
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2026-03-27 11:48:28 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import rotas from "./routes.js";
 const app = express()
 
 // Fala para a aplicação qual arquivo ela deve usar para rotas
-app.use(rotas)
+app.use(express.json())
 
 // Fala para a aplicação que para as rotas do usuarios, deverá haver o prefixo '/usuarios' para depois haver 
 // as outras rotas. Por exemplo, 'Login', será "/usuarios/login"
 // app.use("/usuarios", rotasUsuarios)
 
 
+app.use(rotas)
 app.listen(8000, ()=>{
   console.log("Iniciando o servidor na porta 8000")
 })
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
 
-import mysql, { type RowDataPacket, type Connection } from "mysql2/promise";
+import mysql, {
+  type RowDataPacket,
+  type Connection,
+  type ResultSetHeader,
+} from "mysql2/promise";
 
 const routes = Router();
 
@@ -22,20 +26,38 @@ routes.get("/pessoas", async (req, res) => {
     const [dados, campos] = await connection.execute<IPessoa[]>(
       "SELECT * FROM pessoa",
     );
+
     res.status(200).json(dados);
   } catch (err) {
-    if (connection) {
-      await (connection as Connection).end();
-    }
+    //TODO:
+    console.log(err);
   }
 });
 
-routes.post("/pessoas", async(req,res) => {
-  let {id,nome} = req.body
-  const result =
-    await connection
-    .execute('INSERT INTO pessoa (id,nome) VALUES (?,?)',[3,"Maria"])
-  console.log(result)
+routes.post("/pessoas", async (req, res) => {
+  let { id, nome } = req.body;
+  if (nome == null) {
+    return res.status(500).json({mensagem: "Erro ao passar o nome "})
+  }
+  if (id == null) {
+    return res.status(500).json({mensagem: "Erro ao passar o id "})
+  }
+  try {
+    const [result] = await connection.execute<ResultSetHeader>(
+    "INSERT INTO pessoa VALUES (?,?)",
+    [id, nome],
+  );
+  
+  if (result.affectedRows === 0) 
+    return res.status(500).json({mensagem:"Erro ao inserir"})
+  
+  return res.status(201).json({ mensagem: "Sucesso ao inserir!" });
+
+  } catch (err) {
+    return res.status(500).json({mensagem:"Erro ao inserir", error:err})
+  }
+
+
 });
 
 export default routes;
