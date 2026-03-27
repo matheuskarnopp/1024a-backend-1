5b1f1271355e71d18327312fe389c4f4d405b0d6
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2026-03-27 10:44:46 -0400

    sim

--- src/routes.ts
+++ src/routes.ts
@@ -1,44 +1,41 @@
 import { Router } from "express";
 
-import mysql, { type RowDataPacket, type Connection } from 'mysql2/promise';
+import mysql, { type RowDataPacket, type Connection } from "mysql2/promise";
 
-const routes = Router()
-
-
+const routes = Router();
 
 ////////////
 interface IPessoa extends RowDataPacket {
-  id: number,
-  nome: "string",
+  id: number;
+  nome: "string";
 }
 
 ////////////
 
-
+const connection = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "luademel",
+});
 routes.get("/pessoas", async (req, res) => {
-  //TODO:Fazer arquivo separado para a coneção de banco, mas usando a pool para a coneção 
-  let connection: Connection | null = null
-  // Create the connection to database
   try {
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      database: 'luademel',
-    })
-    const [dados, campos] = await connection.execute<IPessoa[]>('SELECT * FROM pessoa')
-    res.status(200).json(dados)
-  }
-  catch (err) {
+    const [dados, campos] = await connection.execute<IPessoa[]>(
+      "SELECT * FROM pessoa",
+    );
+    res.status(200).json(dados);
+  } catch (err) {
     if (connection) {
-      await (connection as Connection).end()
+      await (connection as Connection).end();
     }
   }
-
 });
 
+routes.post("/pessoas", async(req,res) => {
+  let {id,nome} = req.body
+  const result =
+    await connection
+    .execute('INSERT INTO pessoa (id,nome) VALUES (?,?)',[3,"Maria"])
+  console.log(result)
+});
 
-// routes.post("/pessoas", ()=>{
-
-// })
-
-export default routes
+export default routes;
