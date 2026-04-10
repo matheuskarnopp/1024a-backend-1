a4ccb103825953c4fcadfbe2dd21ce0b134007c4
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2026-04-10 11:08:09 -0400

    criação da verificação de erro para chave primaria duplicada

--- src/service.ts
+++ src/service.ts
@@ -5,6 +5,7 @@ class MysqlErrorHandle{
 
     }
     verificaErroDB(){
+        console.log(this.error)
         if (this.error instanceof Error && 'code' in this.error && (this.error.code === 'ECONNREFUSED' || this.error.code === 'EAI_AGAIN')) {
             this.res.status(500).json({ mensagem: "ERRO: LIGUE O LARAGON e confira o usuário e senha da conexão" })
         } else if (this.error instanceof Error && 'code' in this.error && this.error.code === 'ENOTFOUND') {
@@ -17,7 +18,10 @@ class MysqlErrorHandle{
             this.res.status(500).json({ mensagem: "ERRO: Você tem um erro na sua SQL, confira o Execute" })
         } else if (this.error instanceof Error && 'code' in this.error && this.error.code === 'ER_NO_SUCH_TABLE') {
             this.res.status(500).json({ mensagem: "ERRO: Você digitou o nome da tabela errado, confira o Execute!" })
-        } else {
+        }else if(this.error instanceof Error && 'code' in this.error && this.error.code === ('ER_DUP_ENTRY')){
+            this.res.status(500).json({ mensagem: "ERRO: Já exite um produto com esse id, troque o id" })
+        }
+         else {
             this.res.status(500).json({ mensagem: "ERRO: Desconhecido!" })
         }
     }
