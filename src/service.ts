3fe59161bef5651bbf45b0509a3dee935da5bdd6
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2026-04-10 11:43:31 -0400

    arquivo para verificar o erro no db

--- src/service.ts
+++ src/service.ts
@@ -4,26 +4,37 @@ class MysqlErrorHandle{
     constructor(readonly error: unknown, readonly res:Response){
 
     }
+    ehErroMysql(error:unknown){
+        if (error instanceof Error && 'code' in error) {
+            return true
+        }
+        return false
+    }
     verificaErroDB(){
         console.log(this.error)
-        if (this.error instanceof Error && 'code' in this.error && (this.error.code === 'ECONNREFUSED' || this.error.code === 'EAI_AGAIN')) {
+        if (this.error instanceof Error && 'code' in this.error) {
+            
+        if (this.error.code === 'ECONNREFUSED' || this.error.code === 'EAI_AGAIN') {
             this.res.status(500).json({ mensagem: "ERRO: LIGUE O LARAGON e confira o usuário e senha da conexão" })
-        } else if (this.error instanceof Error && 'code' in this.error && this.error.code === 'ENOTFOUND') {
+        } else if (this.error.code === 'ENOTFOUND') {
             this.res.status(500).json({ mensagem: "ERRO: Você digitou algo errado no host da conexão" })
-        } else if (this.error instanceof Error && 'code' in this.error && this.error.code === 'ER_BAD_DB_ERROR') {
+        } else if (this.error.code === 'ER_BAD_DB_ERROR') {
             this.res.status(500).json({ mensagem: "ERRO: Confira o nome do banco de dados ou crie um banco com o nome que você passou na conexão" })
-        } else if (this.error instanceof Error && 'code' in this.error && this.error.code === 'ER_ACCESS_DENIED_ERROR') {
+        } else if (this.error.code === 'ER_ACCESS_DENIED_ERROR') {
             this.res.status(500).json({ mensagem: "ERRO: Confira usuario e senha na conexão" })
-        } else if (this.error instanceof Error && 'code' in this.error && this.error.code === 'ER_PARSE_ERROR') {
+        } else if (this.error.code === 'ER_PARSE_ERROR') {
             this.res.status(500).json({ mensagem: "ERRO: Você tem um erro na sua SQL, confira o Execute" })
-        } else if (this.error instanceof Error && 'code' in this.error && this.error.code === 'ER_NO_SUCH_TABLE') {
+        } else if (this.error.code === 'ER_NO_SUCH_TABLE') {
             this.res.status(500).json({ mensagem: "ERRO: Você digitou o nome da tabela errado, confira o Execute!" })
-        }else if(this.error instanceof Error && 'code' in this.error && this.error.code === ('ER_DUP_ENTRY')){
+        }else if(this.error.code === ('ER_DUP_ENTRY')){
             this.res.status(500).json({ mensagem: "ERRO: Já exite um id cadastrado anteriormente no banco de dados" })
         }
-         else {
+        else {
             this.res.status(500).json({ mensagem: "ERRO: Desconhecido!" })
         }
+        }
+
+
     }
 }
 
